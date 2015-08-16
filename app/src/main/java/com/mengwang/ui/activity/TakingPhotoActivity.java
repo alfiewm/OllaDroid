560ec30abf9eba1c8a6b7066faf8ad4dca31302d
mwang <alfiewm@gmail.com>
2015-08-16 13:51:26 +0800

    add blank take photo activity

---
+++ app/src/main/java/com/mengwang/ui/activity/TakingPhotoActivity.java
@@ -0,0 +1,9 @@
+package com.mengwang.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mengw on 7/22/15.
+ */
+public class TakingPhotoActivity extends AppCompatActivity {
+}
