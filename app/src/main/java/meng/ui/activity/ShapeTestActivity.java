10cf7aec160b6fd3290456492cfebf4be4a3be5b
mwang <alfiewm@gmail.com>
2015-08-31 12:32:35 +0800

    cdata in string test.

--- app/src/main/java/meng/ui/activity/ShapeTestActivity.java
+++ app/src/main/java/meng/ui/activity/ShapeTestActivity.java
@@ -2,6 +2,8 @@ package meng.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.TextView;
 
 import meng.olladroid.R;
 
@@ -11,5 +13,7 @@ public class ShapeTestActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shape_test);
+        String s = getString(R.string.welcome_to_x, getString(R.string.app_name_reg_title));
+        ((TextView) findViewById(R.id.html_text_view)).setText(Html.fromHtml(s));
     }
 }
