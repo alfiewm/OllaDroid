dfa82cdedfc513c4dc42c2e0f5f5ac7716071331
mwang <mwang@tango.me>
2015-10-28 11:36:01 +0800

    view switcher animation test

--- app/src/main/java/meng/ui/activity/ShapeTestActivity.java
+++ app/src/main/java/meng/ui/activity/ShapeTestActivity.java
@@ -4,10 +4,12 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -25,6 +27,14 @@ public class ShapeTestActivity extends AppCompatActivity {
     PinEntryView pinEntryView;
     @InjectView(R.id.textDialog)
     EditText textInputEditText;
+    @InjectView(R.id.view_switcher)
+    ViewSwitcher viewSwitcher;
+    @InjectView(R.id.toolbar_switcher)
+    ViewSwitcher toolbarSwitcher;
+    @InjectView(R.id.activity_toolbar)
+    Toolbar activityToolbar;
+    @InjectView(R.id.drawer_toolbar)
+    Toolbar drawerToolbar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,8 @@ public class ShapeTestActivity extends AppCompatActivity {
         htmlTextView.setText(Html.fromHtml(s));
         textInputLayout.setHint("Mobile Number");
         textInputEditText.setTextSize(11);
+        activityToolbar.setTitle("Barak Obama");
+        drawerToolbar.setTitle("Maps!");
     }
 
     @Override
@@ -43,7 +55,7 @@ public class ShapeTestActivity extends AppCompatActivity {
         ButterKnife.reset(this);
     }
 
-    @OnClick({R.id.btn_set_pin_error, R.id.btn_clear_pin_error, R.id.btn_clear_text})
+    @OnClick({R.id.btn_set_pin_error, R.id.btn_clear_pin_error, R.id.btn_clear_text, R.id.view_switcher, R.id.toolbar_switcher})
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_set_pin_error:
@@ -54,6 +66,14 @@ public class ShapeTestActivity extends AppCompatActivity {
                 break;
             case R.id.btn_clear_text:
                 pinEntryView.clearText();
+                break;
+            case R.id.view_switcher:
+//                viewSwitcher.showNext();
+//                toolbarSwitcher.showNext();
+                toolbarSwitcher.setDisplayedChild(toolbarSwitcher.getDisplayedChild() == 0 ? 1 : 0);
+                break;
+            case R.id.toolbar_switcher:
+                toolbarSwitcher.showNext();
                 break;
             default:
                 break;
